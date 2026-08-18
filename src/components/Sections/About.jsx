13822cca2efe5a884e6f6176f2048c301cabe2ab
shyamsundar58 <shyamsundarrajan00@gmail.com>
2026-08-19 01:56:22 +0530

    About name change

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -1,7 +1,7 @@
 ﻿import { motion } from "framer-motion";
 
 // About Image
-import aboutImage from "../../assets/about/about1.webp";
+import aboutImage from "../../assets/about/about01.webp";
 
 const stats = [
     {
